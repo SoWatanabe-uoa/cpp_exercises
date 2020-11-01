21f12499ec2fa8aa8d677a56afbcf7575c05738f
s1260231 <52701780+s1260231@users.noreply.github.com>
2020-11-01 14:18:37 +0900

    Update filestreams.cpp

--- ex02/filestreams.cpp
+++ ex02/filestreams.cpp
@@ -4,11 +4,14 @@
 #include <fstream>
 #include <iomanip>
 
+#define MAX 100
+
 using namespace std;
 
 int main(){
-    int col1[5];
-    double col2[5];
+    int col1[MAX];
+    double col2[MAX];
+    int num_lines=0;
 
     //Input from the file
     ifstream input("data.txt");
@@ -16,14 +19,15 @@ int main(){
         cerr << "Cannot open file" << endl;
         exit(1);
     }
-    for(int i=0; i < 5; i++){
-        input >> col1[i] >> col2[i];
+    while( !input.eof() ){
+        input >> col1[num_lines] >> col2[num_lines];
+        num_lines++;
     }
     input.close();
 
     //std output
     cout << setfill('-') << setw(21) << '+' << setw(21) << '+' << setw(21) << ' ' << endl;
-    for(int i=0; i < 5; i++){
+    for(int i=0; i < num_lines; i++){
         cout << setfill(' ') << setw(19) << i+1 << " |" << setw(19) << col1[i] << " |"
         << setw(20) << col2[i] << endl;
     }
