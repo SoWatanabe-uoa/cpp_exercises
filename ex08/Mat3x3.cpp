a2dc64f792b9371055c348af0f047bf2307ae6b0
s1260231 <52701780+s1260231@users.noreply.github.com>
2020-11-12 04:19:16 +0900

    Add files via upload

---
+++ ex08/Mat3x3.cpp
@@ -0,0 +1,101 @@
+#include "Mat3x3.h"
+
+Mat3x3::Mat3x3(){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            entries[i][j] = (i==j) ? 1.0 : 0.0;
+        }
+    }
+}
+
+Mat3x3::Mat3x3(double other_entries[3][3]){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            entries[i][j] = other_entries[i][j];
+        }
+    }
+}
+
+Mat3x3& Mat3x3::operator+= (const Mat3x3& other) {
+    *this = *this + other;
+    return *this;
+}
+
+Mat3x3& Mat3x3::operator-= (const Mat3x3& other) {
+    *this = *this - other;
+    return *this;
+}
+
+Mat3x3& Mat3x3::operator*=(const Mat3x3& other) {
+    *this = (*this) * (other);
+    return *this;
+}
+
+
+const Mat3x3 Mat3x3::operator-() {
+    Mat3x3 result;
+    for (int i=0; i<3; ++i){
+        for (int j=0; j<3; ++j){
+            result.entries[i][j] = -entries[i][j];
+        }
+    }
+    return result;
+}
+
+double& Mat3x3::operator() (int i,int j){
+    return entries[i][j];
+}
+
+bool Mat3x3::operator== (const Mat3x3& other) const {
+    const double eps = 1e-5;
+    bool equal_flag = true;
+    for (int i=0; i<3; ++i){
+        for (int j=0; j<3; ++j){
+            if( !(std::fabs(entries[i][j] - other.entries[i][j]) < eps) ) equal_flag = false;
+        }
+    }
+    return equal_flag;
+}
+
+ostream& operator<< (ostream& os, const Mat3x3& m) {
+    for (int i=0; i<3; ++i) {
+        for (int j=0; j<3; ++j) {
+            os << m.entries[i][j] << " ";
+        }
+        os << endl;
+    }
+    return os;
+}
+
+const Mat3x3 operator+(const Mat3x3& m1,const Mat3x3& m2){
+    Mat3x3 result;
+    for (int i=0; i < 3; ++i){
+        for (int j=0; j < 3; ++j){
+            result.entries[i][j] = m1.entries[i][j] + m2.entries[i][j];
+        }
+    }
+    return result;
+}
+
+const Mat3x3 operator-(const Mat3x3& m1,const Mat3x3& m2){
+    Mat3x3 result;
+    for (int i=0; i < 3; ++i){
+        for (int j=0; j < 3; ++j){
+            result.entries[i][j] = m1.entries[i][j] - m2.entries[i][j];
+        }
+    }
+    return result;
+}
+
+const Mat3x3 operator*(const Mat3x3& m1,const Mat3x3& m2){
+    Mat3x3 result;
+    for (int i=0; i<3; ++i) {
+        for (int j=0; j<3; ++j) {
+            result.entries[i][j] = 0.0;
+            for (int k=0; k<3; ++k) {
+                result.entries[i][j] += m1.entries[i][k] * m2.entries[k][j];
+            }
+        }
+    }
+    return result;
+}
---
+++ ex08/Mat3x3.h
@@ -0,0 +1,28 @@
+#ifndef MAT3X3_h
+#define MAT3X3_h
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Mat3x3 {
+    public:
+    Mat3x3();
+    Mat3x3(double[3][3]);
+    Mat3x3& operator+= (const Mat3x3&);
+    Mat3x3& operator-= (const Mat3x3&);
+    Mat3x3& operator*= (const Mat3x3&);
+    const Mat3x3 operator-();
+    double& operator() (int, int);
+    bool operator== (const Mat3x3&) const;
+    friend ostream& operator<< (ostream& os, const Mat3x3& m);
+
+    double entries[3][3];
+};
+
+const Mat3x3 operator+(const Mat3x3& ,const Mat3x3&);
+const Mat3x3 operator-(const Mat3x3& ,const Mat3x3&);
+const Mat3x3 operator*(const Mat3x3& ,const Mat3x3&);
+
+#endif
